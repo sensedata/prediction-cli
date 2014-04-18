d0052817635ac6da4bb53748697026fc2bb57753
Matthew Jones <matt@joneshq.me>
2014-04-18 11:55:07 -0600

    do not print usage out twice when help requested

--- bin/cmd.js
+++ bin/cmd.js
@@ -17,35 +17,39 @@ var argv = minimist(process.argv.slice(2), {
 
 var csvFile = argv.csv;
 
-if (argv.h || argv.help) usage(0);
+if (argv.h || argv.help) { 
+  usage(0);
 
-var command = argv._[0];
+} else {
+  var command = argv._[0];
 
-switch(command) {
-  case 'analyze':
-    predict.analyze(argv.e, argv.k, argv.p, argv.m, handleResult);
-    break;
-  case 'delete':
-    predict.delete(argv.e, argv.k, argv.p, argv.m, handleResult);
-    break;
-  case 'get':
-    predict.get(argv.e, argv.k, argv.p, argv.m, handleResult);
-    break;
-  case 'insert':
-    predict.insert(argv.e, argv.k, argv.p, argv.m, csvFile, handleResult);
-    break;
-  case 'list':
-    predict.list(argv.e, argv.k, argv.p, handleResult);
-    break;
-  case 'predict':
-    predict.predict(argv.e, argv.k, argv.p, argv.m, argv._[1], handleResult);
-    break;
-  case 'update':
-    predict.update(argv.e, argv.k, argv.p, argv.m, csvFile, handleResult);
-    break;
+  switch(command) {
+    case 'analyze':
+      predict.analyze(argv.e, argv.k, argv.p, argv.m, handleResult);
+      break;
+    case 'delete':
+      predict.delete(argv.e, argv.k, argv.p, argv.m, handleResult);
+      break;
+    case 'get':
+      predict.get(argv.e, argv.k, argv.p, argv.m, handleResult);
+      break;
+    case 'insert':
+      predict.insert(argv.e, argv.k, argv.p, argv.m, csvFile, handleResult);
+      break;
+    case 'list':
+      predict.list(argv.e, argv.k, argv.p, handleResult);
+      break;
+    case 'predict':
+      predict.predict(argv.e, argv.k, argv.p, argv.m, argv._[1], handleResult);
+      break;
+    case 'update':
+      predict.update(argv.e, argv.k, argv.p, argv.m, csvFile, handleResult);
+      break;
     
-  default:
-    usage(1);
+    default:
+      usage(1);      
+  }
+  
 }
 
 function usage (code) {
